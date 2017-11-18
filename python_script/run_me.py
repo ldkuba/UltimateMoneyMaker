ab72883688bc0a7a733d82093e6dd3fc92e76ad3
GeoffreySmythe <vadas113@gmail.com>
2017-11-18 18:02:17 +0000

    python script

---
+++ python_script/run_me.py
@@ -0,0 +1,19 @@
+import datetime
+import pandas as pd
+import os.path
+from forex_python.converter import get_rate
+import time
+from multiprocessing import Process 
+
+def updateDataThreading (counter, startTime, endTime):
+    while (endTime.timestamp () != startTime.timestamp ()):
+        with open (str (counter) + '.csv', 'a') as f:
+            try:
+                rate = get_rate ('GBP', 'USD', startTime)
+                f.write (str (startTime.timestamp ()) + ',' + str (startTime) + ',' + str (rate) + '\n')
+                startTime += datetime.timedelta (hours = 1)
+                print (rate)
+            except Exception as e:
+                pass
+#                                          Change these numbers!
+updateDataThreading (1, datetime.datetime (2001, 1, 1), datetime.datetime (2002, 1, 1))
